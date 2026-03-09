752af7c5c9e7992f6a252a0cb5f083c0dfd08833
Michael Carrillo <bigmickey05@gmail.com>
2026-03-09 09:37:37 -0700

    Transferred Everything from chess-123
    Original chess-123 Repo:
    https://github.com/MCarri34/chess-123

---
+++ classes/Chess.h
@@ -0,0 +1,52 @@
+#pragma once
+
+#include "Game.h"
+#include "Grid.h"
+
+#include <vector>
+#include "Bitboard.h"
+
+constexpr int pieceSize = 80;
+
+class Chess : public Game
+{
+public:
+    Chess();
+    ~Chess();
+
+    void setUpBoard() override;
+
+    bool canBitMoveFrom(Bit &bit, BitHolder &src) override;
+    bool canBitMoveFromTo(Bit &bit, BitHolder &src, BitHolder &dst) override;
+    bool actionForEmptyHolder(BitHolder &holder) override;
+
+    void stopGame() override;
+
+    Player *checkForWinner() override;
+    bool checkForDraw() override;
+
+    std::string initialStateString() override;
+    std::string stateString() override;
+    void setStateString(const std::string &s) override;
+    void bitMovedFromTo(Bit &bit, BitHolder &src, BitHolder &dst) override;
+    bool clickedBit(Bit &bit) override;
+    void drawFrame() override;
+    void clearBoardHighlights() override;
+
+    Grid* getGrid() override { return _grid; }
+    std::vector<BitMove> generateMoves(const char* state, char color);
+    std::vector<BitMove> generateAllMoves(const char* state, char color);
+    
+private:
+    Bit* PieceForPlayer(const int playerNumber, ChessPiece piece);
+    Player* ownerAt(int x, int y) const;
+    void FENtoBoard(const std::string& fen);
+    char pieceNotation(int x, int y) const;
+
+    Grid* _grid;
+
+    std::vector<BitMove> _lastMoves;
+    int _lastFrom = -1;
+
+    bool _highlightsActive = false;
+};
